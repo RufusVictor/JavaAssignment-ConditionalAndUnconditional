2b18d63277aad4a57be10a13e2e219fd5d26ac18
Rufus Victor <139438518+RufusVictor@users.noreply.github.com>
2025-02-27 00:29:54 +0530

    Create PrimeNumbers.java

---
+++ PrimeNumbers.java
@@ -0,0 +1,14 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        for (int num = 2; num <= 30; num++) {
+            boolean isPrime = true;
+            for (int i = 2; i * i <= num; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) System.out.print(num + " ");
+        }
+    }
+}
